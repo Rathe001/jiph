7996f668556b756cdb645d110f666af2981e532f
Josh Tummel <josh@astigmapro.com>
2015-11-08 12:38:55 -0500

    new icons

--- src/js/app/common/services/navigation/navigation.js
+++ src/js/app/common/services/navigation/navigation.js
@@ -6,11 +6,11 @@ angular.module('modCommon').factory('Navigation', [
         service.all = [
             { name: "Dashboard", icon: "fa-area-chart", url: "/", children: [] },
             { name: "Campaigns", icon: "fa-bullhorn", url: "/campaigns", children: [] },
-            { name: "Ad Sets", icon: "fa-bullhorn", url: "/ad-sets", children: [] },
-            { name: "Ads", icon: "fa-bullhorn", url: "/ads", children: [] },
+            { name: "Ad Sets", icon: "fa-folder-open-o", url: "/ad-sets", children: [] },
+            { name: "Ads", icon: "fa-image", url: "/ads", children: [] },
             { name: "Tracking", icon: "fa-bullseye", url: "/tracking", children: [] },
             { name: "Preset Audiences", icon: "fa-users", url: "/preset-audiences", children: [] },
-            { name: "Preset Ad Groups ", icon: "fa-picture-o", url: "/preset-ad-groups", children: [] },
+            { name: "Preset Ad Groups ", icon: "fa-files-o", url: "/preset-ad-groups", children: [] },
             { name: "Automation", icon: "fa-gears", url: "/automation", children: [] },
             { name: "Help", icon: "fa-question-circle", url: "/help", children: [] }
         ];
