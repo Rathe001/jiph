18efc64c50240cd9171607b5fa421cbb215afac0
Josh Tummel <josh@astigmapro.com>
2015-11-18 14:18:10 -0500

    getting pages, posts, pixels

--- src/js/app/campaigns/controllers/create/create.js
+++ src/js/app/campaigns/controllers/create/create.js
@@ -1,21 +1,44 @@
 let modCampaigns = angular.module('modCampaigns');
 
-modCampaigns.controller('ctrlCampaignsCreate', ['Campaigns', 'Dictionary', 'Objectives',
-    function(Campaigns, Dictionary, Objectives) {
+modCampaigns.controller('ctrlCampaignsCreate', ['$scope', 'Accounts', 'Campaigns', 'Dictionary', 'Objectives', 'Facebook',
+    function($scope, Accounts, Campaigns, Dictionary, Objectives, Facebook) {
         let vm = this;
 
         vm.objectives = [];
         vm.dictionary = {};
         vm.campaign = {};
+        vm.pages = [];
+        vm.posts = [];
+        vm.pixels = [];
 
         vm.showDetails = showDetails;
 
         _init();
 
+        $scope.$watch(() => vm.campaign.objective, newVal => {
+            if(newVal) {
+                //console.log(newVal);
+            }
+        });
+
+        $scope.$watch(() => Accounts.active, newVal => {
+            if(newVal) {
+                _init();
+            }
+        });
+
+        $scope.$watch(() => vm.campaign.promoted_object.page_id, newVal => {
+            if(newVal && vm.campaign.objective === "POST_ENGAGEMENT") {
+                _getPagePosts();
+            }
+        });
+
         function _init(){
             vm.objectives = Objectives.all;
             vm.dictionary = Dictionary;
             vm.campaign = _setDefaultCampaign();
+            vm.pages = _getAccountPages();
+            vm.pixels = _getAccountPixels();
         }
 
         function _setDefaultCampaign() {
@@ -23,11 +46,43 @@ modCampaigns.controller('ctrlCampaignsCreate', ['Campaigns', 'Dictionary', 'Obje
                 name: "",
                 objective: "",
                 buying_type: "AUCTION",
-                promoted_object: {},
-                spend_cap: null,
-                execution_options: "VALIDATE_ONLY",
-                adlabels: {}
+                promoted_object: {
+                    page_id: undefined,
+                    application_id: undefined,
+                    pixel_id: undefined,
+                    custom_event_type: undefined,
+                    object_store_url: undefined,
+                    offer_id: undefined,
+                    product_catalog_id: undefined,
+                    product_set_id: undefined
+                },
+                spend_cap: undefined,
+                execution_options: undefined,
+                adlabels: undefined
             }
+        }
+
+        function _getAccountPages() {
+            Facebook.get('/' + Accounts.active + '/connectionobjects').then(connectionobjects => {
+                let ary = connectionobjects.data;
+                vm.pages = ary.filter(obj => obj.type === 1);
+                vm.applications = ary.filter(obj => obj.type === 2);
+                vm.events = ary.filter(obj => obj.type === 3);
+                vm.places = ary.filter(obj => obj.type === 6);
+                vm.domains = ary.filter(obj => obj.type === 7);
+            });
+        }
+
+        function _getAccountPixels() {
+            Facebook.get('/' + Accounts.active + '/offsitepixels').then(offsitepixels => {
+                vm.pixels = offsitepixels.data;
+            });
+        }
+
+        function _getPagePosts() {
+            Facebook.get('/' + vm.campaign.promoted_object.page_id + '/promotable_posts', {fields:["full_picture","icon","is_published","link","message","name","object_id","picture","story","type"]}).then(posts => {
+                vm.posts = posts.data;
+            });
         }
 
         function showDetails() {
---
+++ src/js/app/common/directives/combo-box/combo-box.js
@@ -0,0 +1,85 @@
+var modCommon = angular.module('modCommon');
+
+modCommon.directive('comboBox', [function () {
+    return {
+        restrict: 'EA',
+        scope: {},
+        bindToController: {
+            comboBox: '=',
+            bind: '=',
+            type: '@',
+            placeholder: '@'
+        },
+        controller: controller,
+        link: link,
+        controllerAs: "vm",
+        replace: true,
+        template: `
+            <div>
+                <div class="click-catcher" ng-click="vm.toggleMenu()" ng-if="vm.showMenu"></div>
+                <div class="combo-box" ng-switch="vm.type">
+                    <div class="wrapper" ng-class="{'loading': !vm.comboBox}">
+                        <input type="text" ng-model="vm.filter" ng-click="vm.toggleMenu()" placeholder="{{vm.placeholder}}" />
+                    </div>
+                    <div class="drop-menu" ng-if="vm.showMenu" ng-switch-when="connectionobjects">
+                        <ul>
+                            <li ng-repeat="c in vm.comboBox | filter:vm.filter" ng-click="vm.select(c)">
+                            <img ng-src="{{c.picture}}" ng-alt="{{c.name}}" />
+                                <div class="pad-content">
+                                    {{c.name}}<br />
+                                    <small ng-if="c.website">{{c.website}}</small>
+                                    <small ng-if="!c.website">{{c.url}}</small>
+                                </div>
+                            </li>
+                            <li ng-if="vm.comboBox.length === 0">No data found.</li>
+                        </ul>
+                    </div>
+                    <div class="drop-menu" ng-if="vm.showMenu" ng-switch-when="pixels">
+                        <ul>
+                            <li ng-repeat="c in vm.comboBox | filter:vm.filter" ng-click="vm.select(c)">
+                                {{c.name}}<br />
+                                <small>ID: {{c.id}} &nbsp;&nbsp;&nbsp; Category: {{c.tag}}</small>
+                            </li>
+                            <li ng-if="vm.comboBox.length === 0">No data found.</li>
+                        </ul>
+                    </div>
+                    <div class="drop-menu" ng-if="vm.showMenu" ng-switch-default>
+                        <ul>
+                            <li ng-repeat="c in vm.comboBox | filter:vm.filter" ng-click="vm.select(c)">{{c.name}}</li>
+                            <li ng-if="vm.comboBox.length === 0">No data found.</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        `
+    };
+
+    function controller() {
+        let vm = this;
+
+        vm.showMenu = false;
+
+        vm.select = select;
+        vm.toggleMenu = toggleMenu;
+
+        function select(c) {
+            vm.bind = c.id;
+            vm.bindName = c.name;
+            vm.filter = vm.bindName;
+            toggleMenu();
+        }
+
+        function toggleMenu() {
+            vm.showMenu = !vm.showMenu;
+            if(vm.showMenu) {
+                vm.filter = "";
+            } else {
+                vm.filter = vm.bindName;
+            }
+        }
+    }
+
+    function link(scope, elem, attrs) {
+
+    }
+}]);
